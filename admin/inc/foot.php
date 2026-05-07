e1ac18b0df02eb1ba5ed32a0aa73d8ff72d3e037
Hanif Pramono <hanif.pram@gmail.com>
2026-05-07 13:08:26 +0700

    Delete admin directory

--- admin/inc/foot.php
+++
@@ -1,7 +0,0 @@
-</div><!-- /main-content -->
-</div><!-- /layout -->
-<footer style="text-align: right;padding: 16px 40px;font-size:12px;color:hsl(215 16% 47%)">
-  Dibuat Oleh <a href="https://hanifprm.my.id" target="_blank" style="color:hsl(217.2 91.2% 59.8%);text-decoration:none;font-weight:600">Hanif Pramono</a>
-</footer>
-</body>
-</html>
--- admin/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once __DIR__ . '/inc/bootstrap.php';
-header('Location: dashboard.php');
-exit;
